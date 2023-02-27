52deb55d6346249cafc998f56decb628a72726e6
Patrick Robichaud <patrick_robichaud@hotmail.com>
2023-02-27 17:20:19 -0500

    Update MultiCaseTest base classes to work without parameters.

--- TestTools/Base/BaseMultiCaseFileTest.cs
+++ TestTools/Base/BaseMultiCaseFileTest.cs
@@ -3,8 +3,53 @@ using Xunit.Abstractions;
 
 namespace TestTools.Base
 {
-    public abstract class BaseMultiCaseFileTest<Parameters, Case, Input> : BaseTest<Parameters>
+    public abstract class BaseMultiCaseFileTest<Case, Input> : BaseTest
         where Case : List<(Input Input, string Path)>
+    {
+        public BaseMultiCaseFileTest(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        public void TestAssertFile(Input input, string expected)
+        {
+            var result = Run(input);
+            _helper.AssertByte(result, expected);
+        }
+
+        public void TestAssertFile(Case scenario)
+        {
+            var results = TestScenario(scenario);
+            _helper.AssertByte(results);
+        }
+
+        public void TestAssertFile(List<Case> scenarios)
+        {
+            var results = new List<(byte[], string)>();
+            foreach (var scenario in scenarios)
+            {
+                var result = TestScenario(scenario);
+                results.AddRange(result);
+            }
+            _helper.AssertByte(results);
+        }
+
+        protected abstract byte[] Run(Input input);
+
+        private List<(byte[], string)> TestScenario(Case scenario)
+        {
+            var results = new List<(byte[], string)>();
+            foreach (var c in scenario)
+            {
+                var result = Run(c.Input);
+                results.Add((result, c.Path));
+            }
+            return results;
+        }
+    }
+
+    public abstract class BaseMultiCaseFileTest<Parameters, Case, Input> : BaseTest<Parameters>
+      where Case : List<(Input Input, string Path)>
     {
         public BaseMultiCaseFileTest(Parameters parameters, ITestOutputHelper output)
             : base(parameters, output)
--- TestTools/Base/BaseMultiCaseTest.cs
+++ TestTools/Base/BaseMultiCaseTest.cs
@@ -5,6 +5,137 @@ using Xunit.Abstractions;
 
 namespace TestTools.Base
 {
+    public abstract class BaseMultiCaseTest<Case, Input, Output> : BaseTest
+        where Case : List<(Input, Output)>
+    {
+        public BaseMultiCaseTest(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        public void TestAssert(Input input, Output expected)
+        {
+            var result = Run(input);
+            var success = Validate(expected, result);
+            var message = success ? string.Empty : GetCaseMessage((expected, result, 0));
+            _output.WriteLine(message);
+            Assert.True(success);
+        }
+
+        public void TestAssert(Case scenario)
+        {
+            var failures = TestScenario(scenario);
+            var message = GetScenarioMessage(failures);
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{scenario.Count - failures.Count}/{scenario.Count} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenario.Count} cases failed.\n{message}");
+            Assert.True(failures.Count == 0);
+        }
+
+        public void TestAssert(List<Case> scenarios)
+        {
+            var failures = TestScenarios(scenarios);
+            var message = GetScenariosMessage(failures);
+            var caseFailures = failures.Sum(x => x.Case.Count);
+            var caseTotal = scenarios.Sum(x => x.Count);
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{caseTotal - caseFailures}/{caseTotal} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenarios.Count} scenarios & {caseFailures}/{caseTotal} cases failed.\n{message}");
+            Assert.True(failures.Count == 0);
+        }
+
+        public void TestAssertFile(Input input, Output expected)
+        {
+            var result = Run(input);
+            var success = Validate(expected, result);
+            var message = success ? string.Empty : GetCaseMessage((expected, result, 0));
+            _output.WriteLine(message);
+            Assert.True(success);
+        }
+
+        public void TestAssertFile(Case scenario)
+        {
+            var failures = TestScenario(scenario);
+            var message = GetScenarioMessage(failures);
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{scenario.Count - failures.Count}/{scenario.Count} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenario.Count} cases failed.\n{message}");
+            Assert.Empty(failures);
+        }
+
+        protected abstract Output Run(Input input);
+
+        private bool Validate(Output expected, Output result)
+        {
+            var e = Serialize(expected);
+            var r = Serialize(result);
+            var output = e == r;
+            return output;
+        }
+
+        protected abstract string Serialize(Output output);
+
+        private IList<(IList<(Output Expected, Output Result, int Case)> Case, int Scenario)>
+          TestScenarios(IList<Case> scenarios)
+        {
+            var failedScenarios = new List<(IList<(Output Expected, Output Result, int Case)> Case, int Scenario)>();
+            for (int i = 0; i < scenarios.Count; ++i)
+            {
+                var scenario = scenarios[i];
+                var failures = TestScenario(scenario);
+                if (failures.Count > 0)
+                {
+                    failedScenarios.Add((failures, i));
+                }
+            }
+
+            return failedScenarios;
+        }
+
+        private IList<(Output Expected, Output Result, int Case)> TestScenario(IList<(Input Input, Output Expected)> scenario)
+        {
+            var failures = new List<(Output Expected, Output Result, int Case)>();
+            for (int i = 0; i < scenario.Count; ++i)
+            {
+                var test = scenario[i];
+                var result = Run(test.Input);
+                var correct = Validate(test.Expected, result);
+                if (!correct)
+                {
+                    failures.Add((test.Expected, result, i));
+                }
+            }
+
+            return failures;
+        }
+
+        private string GetScenariosMessage(IList<(IList<(Output Expected, Output Result, int Case)> Case, int Scenario)> failures)
+        {
+            var message = string.Empty;
+            foreach (var failure in failures)
+            {
+                message += $"Scenario #{failure.Scenario + 1}: {failure.Case.Count} failures.\n";
+                message += GetScenarioMessage(failure.Case);
+            }
+            return message;
+        }
+
+        private string GetScenarioMessage(IList<(Output Expected, Output Result, int Case)> failures)
+        {
+            var message = string.Empty;
+            foreach (var failure in failures)
+            {
+                message += GetCaseMessage(failure);
+            }
+            return message;
+        }
+
+        private string GetCaseMessage((Output Expected, Output Result, int Case) failure)
+        {
+            var expected = Serialize(failure.Expected);
+            var result = Serialize(failure.Result);
+            var message = $"  Case #{failure.Case + 1}: expected [{expected}], but got [{result}].\n";
+            return message;
+        }
+    }
+
     public abstract class BaseMultiCaseTest<Parameters, Case, Input, Output> : BaseTest<Parameters>
         where Case : List<(Input, Output)>
     {
@@ -26,7 +157,8 @@ namespace TestTools.Base
         {
             var failures = TestScenario(scenario);
             var message = GetScenarioMessage(failures);
-            _output.WriteLine($"{failures.Count} cases failed.\n{message}");
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{scenario.Case.Count - failures.Count}/{scenario.Case.Count} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenario.Case.Count} cases failed.\n{message}");
             Assert.True(failures.Count == 0);
         }
 
@@ -34,7 +166,10 @@ namespace TestTools.Base
         {
             var failures = TestScenarios(scenarios);
             var message = GetScenariosMessage(failures);
-            _output.WriteLine($"{failures.Count} scenarios & {failures.Sum(x => x.Case.Count)} cases failed.\n{message}");
+            var caseFailures = failures.Sum(x => x.Case.Count);
+            var caseTotal = scenarios.Sum(x => x.Case.Count);
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{caseTotal - caseFailures}/{caseTotal} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenarios.Count} scenarios & {caseFailures}/{caseTotal} cases failed.\n{message}");
             Assert.True(failures.Count == 0);
         }
 
@@ -51,7 +186,8 @@ namespace TestTools.Base
         {
             var failures = TestScenario(scenario);
             var message = GetScenarioMessage(failures);
-            _output.WriteLine($"{failures.Count} cases failed.\n{message}");
+            _output.WriteLine($"{(failures.Count == 0 ? "ALL " : "")}{scenario.Case.Count - failures.Count}/{scenario.Case.Count} cases passed.");
+            if (failures.Count > 0) _output.WriteLine($"{failures.Count}/{scenario.Case.Count} cases failed.\n{message}");
             Assert.Empty(failures);
         }
 
